edc65cab4a589e41601dd793616fa9ee6b0e4ded
lamido <kinglamido@gmail.com>
2020-04-09 17:48:51 +0100

    finish challenge 1,2,3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,86 @@
-const covid19ImpactEstimator = (data) => data;
+const infectionsBRT = (timeToElapse, currentlyInfected) => {
+  const factor = timeToElapse / 3;
+
+  const every3Days = 2 ** factor;
+
+  return currentlyInfected * every3Days;
+};
+
+const severeCasesBRT = (InfectionsByRequestedTime) => (InfectionsByRequestedTime / 100) * 15;
+
+const hospitalBedsBRT = (totalHospitalBeds, severeCasesByRequestedTime) => {
+  const availableBed = (totalHospitalBeds / 100) * 35;
+
+  return availableBed - severeCasesByRequestedTime;
+};
+
+const casesForICUBRT = (InfectionsByRequestedTime) => (InfectionsByRequestedTime / 100) * 5;
+
+const casesForVentilatorsBRT = (InfectionsByRequestedTime) => (InfectionsByRequestedTime / 100) * 2;
+
+const dollarsInFlightBRT = (InfectionsByRequestedTime, data) => {
+  const { region, timeToElapse } = data;
+  const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = region;
+
+  return InfectionsByRequestedTime * avgDailyIncomePopulation * avgDailyIncomeInUSD * timeToElapse;
+};
+
+const covid19ImpactEstimator = (data) => {
+  const currentlyInfectedI = data.reportedCases * 10;
+
+  const currentlyInfectedSI = data.reportedCases * 50;
+
+  const infectionsByRequestedTimeI = infectionsBRT(data.timeToElapse, currentlyInfectedI);
+
+  const infectionsByRequestedTimeSI = infectionsBRT(data.timeToElapse, currentlyInfectedSI);
+
+  const severeCasesByRequestedTimeI = severeCasesBRT(infectionsByRequestedTimeI);
+
+  const severeCasesByRequestedTimeSI = severeCasesBRT(infectionsByRequestedTimeSI);
+
+  const hospitalBedsByRTI = hospitalBedsBRT(data.totalHospitalBeds, severeCasesByRequestedTimeI);
+
+  const hospitalBedsByRTSI = hospitalBedsBRT(data.totalHospitalBeds, severeCasesByRequestedTimeSI);
+
+  const casesForICUByRTI = casesForICUBRT(infectionsByRequestedTimeI);
+
+  const casesForICUByRTSI = casesForICUBRT(infectionsByRequestedTimeSI);
+
+  const casesForVentilatorsByRTI = casesForVentilatorsBRT(infectionsByRequestedTimeI);
+
+  const casesForVentilatorsByRTSI = casesForVentilatorsBRT(infectionsByRequestedTimeSI);
+
+  const dollarsInFlightByRTI = dollarsInFlightBRT(infectionsByRequestedTimeI, data);
+
+  const dollarsInFlightByRTSI = dollarsInFlightBRT(infectionsByRequestedTimeSI, data);
+
+  const impact = {
+    currentlyInfected: currentlyInfectedI,
+    infectionsByRequestedTime: infectionsByRequestedTimeI,
+    severeCasesByRequestedTime: severeCasesByRequestedTimeI,
+    hospitalBedsByRequestedTime: hospitalBedsByRTI,
+    casesForICUByRequestedTime: casesForICUByRTI,
+    casesForVentilatorsByRequestedTime: casesForVentilatorsByRTI,
+    dollarsInFlight: dollarsInFlightByRTI
+  };
+
+  const severeImpact = {
+    currentlyInfected: currentlyInfectedSI,
+    infectionsByRequestedTime: infectionsByRequestedTimeSI,
+    severeCasesByRequestedTime: severeCasesByRequestedTimeSI,
+    hospitalBedsByRequestedTime: hospitalBedsByRTSI,
+    casesForICUByRequestedTime: casesForICUByRTSI,
+    casesForVentilatorsByRequestedTime: casesForVentilatorsByRTSI,
+    dollarsInFlight: dollarsInFlightByRTSI
+  };
+
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
+
 
 export default covid19ImpactEstimator;
