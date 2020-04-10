8e21af4e497494a1e5ded5b2d66f168b4f112660
lamido <kinglamido@gmail.com>
2020-04-10 15:29:08 +0100

    bg-8 challenge

--- src/estimator.js
+++ src/estimator.js
@@ -31,12 +31,12 @@ const hospitalBedsBRT = (totalHospitalBeds, severeCasesByRequestedTime) => {
 };
 
 const casesForICUBRT = (InfectionsByRequestedTime) => {
-  const casesForICU = Math.trunc((InfectionsByRequestedTime / 100) * 5);
+  const casesForICU = (InfectionsByRequestedTime / 100) * 5;
   return casesForICU;
 };
 
 const casesForVentilatorsBRT = (InfectionsByRequestedTime) => {
-  const casesForVentilators = Math.trunc((InfectionsByRequestedTime / 100) * 2);
+  const casesForVentilators = (InfectionsByRequestedTime / 100) * 2;
   return casesForVentilators;
 };
 
