a2f7463fde8b3ff98eb9ff49e428265b67d3b1de
lamido <kinglamido@gmail.com>
2020-04-10 18:15:21 +0100

    bg-9 challenge

--- src/estimator.js
+++ src/estimator.js
@@ -31,12 +31,12 @@ const hospitalBedsBRT = (totalHospitalBeds, severeCasesByRequestedTime) => {
 };
 
 const casesForICUBRT = (InfectionsByRequestedTime) => {
-  const casesForICU = parseInt(InfectionsByRequestedTime / 100, 10) * 5;
+  const casesForICU = (InfectionsByRequestedTime / 100) * 5;
   return casesForICU;
 };
 
 const casesForVentilatorsBRT = (InfectionsByRequestedTime) => {
-  const casesForVentilators = parseInt(InfectionsByRequestedTime / 100, 10) * 2;
+  const casesForVentilators = Math.floor(InfectionsByRequestedTime / 100) * 2;
   return (casesForVentilators);
 };
 
